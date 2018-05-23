ce9eae105cbee6a98cd2881bf10e8e95c7ddaed5
Adrian Alvarez <adrian426alv@gmail.com>
2018-05-22 21:22:02 -0600

    Making Waater

--- code/threads/threadtest.cc
+++ code/threads/threadtest.cc
@@ -14,8 +14,15 @@
 #include "copyright.h"
 #include "system.h"
 #include "dinningph.h"
+#include "synch.h"
 
 DinningPh * dp;
+Semaphore* sO;
+Semaphore* sH;
+Lock* mutex;
+int cO = 0;
+int cH = 0;
+
 
 void Philo( void * p ) {
 
@@ -43,8 +50,6 @@ void Philo( void * p ) {
     }
 
 }
-
-
 //----------------------------------------------------------------------
 // SimpleThread
 // 	Loop 10 times, yielding the CPU to another ready thread
@@ -53,7 +58,6 @@ void Philo( void * p ) {
 //	"name" points to a string with a thread name, just for
 //      debugging purposes.
 //----------------------------------------------------------------------
-
 void
 SimpleThread(void* name)
 {
@@ -64,14 +68,14 @@ SimpleThread(void* name)
     // the code will behave incorrectly, because
     // printf execution may cause race conditions.
     for (int num = 0; num < 10; num++) {
-        //IntStatus oldLevel = interrupt->SetLevel(IntOff);
+        IntStatus oldLevel = interrupt->SetLevel(IntOff);
 	printf("*** thread %s looped %d times\n", threadName, num);
-	//interrupt->SetLevel(oldLevel);
-        //currentThread->Yield();
+	interrupt->SetLevel(oldLevel);
+        currentThread->Yield();
     }
-    //IntStatus oldLevel = interrupt->SetLevel(IntOff);
+    IntStatus oldLevel = interrupt->SetLevel(IntOff);
     printf(">>> Thread %s has finished\n", threadName);
-    //interrupt->SetLevel(oldLevel);
+    interrupt->SetLevel(oldLevel);
 }
 
 
@@ -82,13 +86,54 @@ SimpleThread(void* name)
 //	ten threads which call SimpleThread, and finally calling
 //	SimpleThread ourselves.
 //----------------------------------------------------------------------
+void
+addH(void* arg)
+{
+
+  long i = (long) arg;
+  mutex->Acquire();
+  if ( (cH > 0)  && (cO > 0) ) {	// I'm an H, have we the others?
+      cH--;	// Using an H
+      cO--;	// Using an O
+      printf("Soy un H, hice agua: %ld\n",i);
+      mutex->Release();		//Free the mutex
+      sH->V();		// Free the waiting thread
+      sO->V();		// Free the waiting thread
+   } else {
+      cH++;			// Increment the H count
+      printf("Soy un H, estoy esperando: %ld\n",i);
+      mutex->Release();		//Free the mutex
+      sH->P();		// Wait for the others
+   }
+
+}
+
+void
+addO(void* arg)
+{
+
+   long i = (long) arg;
+   mutex->Acquire();	// Lock access to cH and cO variables
+   if ( cH > 1 ) {	// I'm an O, have we at least two H's?
+      cH-=2;		// We use two H's
+      printf("Soy un O, hice agua: %ld\n",i);
+      mutex->Release();	//Free the mutex
+      sH->V();	// Free the waiting threads
+      sH->V();
+   } else {
+      cO++;		// Increment O count, then wait
+      printf("Soy un O, estoy esperando: %ld\n",i);
+      mutex->Release();	//Free the mutex
+      sO->P();	// Wait for other threads
+   }
+
+}
+
 
 void
 ThreadTest()
-{
-    Thread * Ph;
-
-    DEBUG('t', "Entering SimpleTest");
+{/*Thread * Ph;
+  DEBUG('t', "Entering SimpleTest");
 
 
     dp = new DinningPh();
@@ -97,8 +142,20 @@ ThreadTest()
         Ph = new Thread( "dp" );
         Ph->Fork( Philo, (void *) k );
     }
-
     return;
+*/
+    sO = new Semaphore("O",1);
+    sH = new Semaphore("H",1);
+    mutex = new Lock("L");
+
+    for ( long k = 0; k < 42; k++ ) {
+      Thread *newThread = new Thread("HO");
+        if((rand() % 2) == 0){
+          newThread->Fork(addH, (void*) k);
+        } else {
+          newThread->Fork(addO, (void*) k);
+        }
+    }
 /*
     for ( int k=1; k<=5; k++) {
       char* threadname = new char[100];
@@ -106,6 +163,6 @@ ThreadTest()
       Thread* newThread = new Thread (threadname);
       newThread->Fork (SimpleThread, (void*)threadname);
     }
-
-    SimpleThread( (void*)"Hilo 0");*/
+        SimpleThread( (void*)"Hilo 0");
+*/
 }
